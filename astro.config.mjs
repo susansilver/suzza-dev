8ecc1004a8286f703228f28fdffa8814c19e6311
Suzza <hello+git@susansilver.net>
2023-10-07 14:41:42 -0700

    convert to starlight docs and add tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,38 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import starlight from '@astrojs/starlight';
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [starlight({
+    title: 'Docs with Tailwind',
+		customCss: [
+				// Path to your Tailwind base styles:
+				'./src/styles/tailwind.css',
+			],
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), 
+  tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false,
+  }),
+    ]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,36 +1,7 @@
-import { z, defineCollection } from "astro:content";
-const blogSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
-});
-
-const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing: z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
-});
-
-export type BlogSchema = z.infer<typeof blogSchema>;
-export type StoreSchema = z.infer<typeof storeSchema>;
-
-const blogCollection = defineCollection({ schema: blogSchema });
-const storeCollection = defineCollection({ schema: storeSchema });
+import { defineCollection } from 'astro:content';
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-    'blog': blogCollection,
-    'store': storeCollection
-}
+	docs: defineCollection({ schema: docsSchema() }),
+	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+};
