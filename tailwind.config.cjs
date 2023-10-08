e512151f322a0202027f9e067b36d9a206cc718e
Suzza <hello+git@susansilver.net>
2023-10-07 20:50:16 -0700

    update acceent colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
-
 const starlightPlugin = require('@astrojs/starlight-tailwind');
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
+				accent: colors.rose,
+				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+				gray: colors.slate,
+			},
+		},
 	},
 	plugins: [starlightPlugin()],
 }
